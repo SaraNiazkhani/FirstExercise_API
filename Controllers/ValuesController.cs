fbfb5dd1295dc528292443a67d9b6d820c73b0e7
sara niaz <saraniazkhani.811012@gmail.com>
2023-10-14 12:22:27 -0700

    lastChange

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,30 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstExercise_API.Models;
+using Microsoft.AspNetCore.Mvc;
 using RestSharp;
 namespace FirstExercise_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
-    {
+    { 
 
-        [HttpGet]
-        public IActionResult GetUserInfo()
-        {
-            Console.WriteLine("Please Enter FirstName: ");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Please Enter LastName:");
-            string lastName = Console.ReadLine();
-            string fullName = ($"FullName:{firstName} + ''+{lastName}");
-            Random random = new Random();
-            int age = random.Next(1, 120);
-            string[] addresses = { "تهران ،خ بهشتی ،کوچه دهم،", "اصفهان،بلوار معلم", "مشهد،خ مدرس" };
-            string address = addresses[random.Next(addresses.Length)];
-            var options = new RestClientOptions("https://eample.com/api");
-            var client = new RestClient(options);
-            var request = new RestRequest("", Method.Get);
-            RestResponse response = client.Execute(request);
-            return Ok(response);
+            [HttpGet]
+            public IActionResult GetUserInfo(string firstName, string lastName)
+            {
+
+
+                Random random = new Random();
+                int age = random.Next(1, 120);
+                string[] addresses = { "تهران ،خ بهشتی ،کوچه دهم،", "اصفهان،بلوار معلم", "مشهد،خ مدرس" };
+                string address = addresses[random.Next(addresses.Length)];
+                var options = new RestClientOptions("https://reqres.in");
+                List<Person> persons = new List<Person>();
+                Person person = new Person();
+                person.FullName = ($"{firstName} {lastName}");
+                person.Address = address;
+                person.Age = age;
+                persons.Add(person);
+
+
+                return Ok(persons);
+
+            }
 
         }
     }
-}
+
---
+++ Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstExercise_API.Models
+{
+    public class Person
+    {
+        
+            public string FullName { get; set; }
+            public int Age { get; set; }
+            public string Address { get; set; }
+        
+    }
+}
+
