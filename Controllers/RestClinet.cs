2f82cc687feeedc860e7b11d98979500acb9e86f
Sara <Sara@DESKTOP-KDK33RI>
2023-10-13 22:53:38 -0700

    Add project files.

---
+++ Controllers/RestClinet.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstExercise_API.Controllers
+{
+    internal class RestClinet
+    {
+        private string v;
+
+        public RestClinet(string v)
+        {
+            this.v = v;
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+namespace FirstExercise_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        
+
+        [HttpGet]
+        public IActionResult GetUserInfo (Method Method)
+        {
+            Console.WriteLine("Please Enter FirstName: ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please Enter LastName:");
+            string lastName = Console.ReadLine();
+            string fullName = ($"FullName:{firstName} + ''+{lastName}" );
+            Random random = new Random();
+            int age = random.Next(1, 120);
+            string[] addresses = { "تهران ،خ بهشتی ،کوچه دهم،", "اصفهان،بلوار معلم", "مشهد،خ مدرس" };
+            string address = addresses[random.Next(addresses.Length)];
+            var options = new RestClinetOptions("https://reqres.in");
+            var request =new RestRequest ("/api",Method.Get);
+            request.AddParameter("FullName", firstName + "" + lastName);
+            request.AddParameter("Address", address);
+            request.AddParameter("Age", age);
+            //var response=client.Equals(request);
+            RestResponse response = client.Execute(request);
+            //var options = new RestClientOptions("https://reqres.in");
+            //var client = new RestClient(options);
+            //var request = new RestRequest("/api/users?page=2", Method.Get);
+            //return Ok(response.Content);
+
+
+
+        }
+
+    }
+}
