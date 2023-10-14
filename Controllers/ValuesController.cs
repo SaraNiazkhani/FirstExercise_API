59f257a1c0dc60dfc3ad11b02da68a3a489c6571
unknown <saraniazkhani.811012@gmail.com>
2023-10-14 18:09:04 +0330

    change two pages

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -6,35 +6,25 @@ namespace FirstExercise_API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        
 
         [HttpGet]
-        public IActionResult GetUserInfo (Method Method)
+        public IActionResult GetUserInfo()
         {
             Console.WriteLine("Please Enter FirstName: ");
             string firstName = Console.ReadLine();
             Console.WriteLine("Please Enter LastName:");
             string lastName = Console.ReadLine();
-            string fullName = ($"FullName:{firstName} + ''+{lastName}" );
+            string fullName = ($"FullName:{firstName} + ''+{lastName}");
             Random random = new Random();
             int age = random.Next(1, 120);
             string[] addresses = { "تهران ،خ بهشتی ،کوچه دهم،", "اصفهان،بلوار معلم", "مشهد،خ مدرس" };
             string address = addresses[random.Next(addresses.Length)];
-            var options = new RestClinetOptions("https://reqres.in");
-            var request =new RestRequest ("/api",Method.Get);
-            request.AddParameter("FullName", firstName + "" + lastName);
-            request.AddParameter("Address", address);
-            request.AddParameter("Age", age);
-            //var response=client.Equals(request);
+            var options = new RestClientOptions("https://eample.com/api");
+            var client = new RestClient(options);
+            var request = new RestRequest("", Method.Get);
             RestResponse response = client.Execute(request);
-            //var options = new RestClientOptions("https://reqres.in");
-            //var client = new RestClient(options);
-            //var request = new RestRequest("/api/users?page=2", Method.Get);
-            //return Ok(response.Content);
-
-
+            return Ok(response);
 
         }
-
     }
 }
